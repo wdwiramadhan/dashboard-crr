dc640d884c7fbf9b922441b8aa68ae055c718bf8
wdwiramadhan <wahyu_dr71@yahoo.com>
2019-11-14 07:43:44 +0700

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Auth from './modules/auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules:{
+    Auth,
+  },
   state: {
+    token: localStorage.getItem('token'),
+    errors:[]
+  },
+  getters:{
+    isAuth: state => {
+      return state.token != "null" && state.token != null
+    }
   },
   mutations: {
+    SET_TOKEN(state, payload) {
+      state.token = payload
+    },
+    SET_ERRORS(state, payload) {
+      state.errors = payload
+    },
+    CLEAR_ERRORS(state) {
+      state.errors = []
+    }
   },
   actions: {
-  },
-  modules: {
   }
 })
