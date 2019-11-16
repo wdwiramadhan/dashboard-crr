41e312ef0814ffd7e667d814aa001bb6dde197f0
wdwiramadhan <wahyu_dr71@yahoo.com>
2019-11-16 17:59:59 +0700

    add navigation bottom

---
+++ src/components/BottomNavigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-bottom-navigation
+     absolute
+    color="deep-purple accent-4"
+  >
+    <v-btn>
+      <span>Recents</span>
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Favorites</span>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Nearby</span>
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
