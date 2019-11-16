70db54749bb6a4a30fb213383aaaeee1f3dc446f
wdwiramadhan <wahyu_dr71@yahoo.com>
2019-11-16 18:00:30 +0700

    fix navbar timeline

--- src/components/NavbarTimeline.vue
+++ src/components/NavbarTimeline.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <v-app-bar  class="primary mb-5">
-      <v-toolbar-title class="white--text">Constraction Realtime Report</v-toolbar-title>
+    <v-app-bar class="deep-purple mb-5">
+      <v-app-bar-nav-icon class="white--text"></v-app-bar-nav-icon>
+      <v-toolbar-title class="white--text">crr</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-icon class="white--text">mdi-bell</v-icon>
     </v-app-bar>    
