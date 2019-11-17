ddb1e5c071edae7f1826d0cd057568144b903075
haqqer <ilhamserbaguna@gmail.com>
2019-11-17 11:27:17 +0700

    modify vue-config

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ],
-  pwa: {
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-        // swSrc is required in InjectManifest mode.
-        swSrc: 'public/service-worker.js',
-        // ...other Workbox options...
-    }
-}
+  ]
 }
